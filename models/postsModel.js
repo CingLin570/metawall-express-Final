d4d6f9eac12111f3f2c5d7ac4343dfdbe9daf69c
Johnny <asd28618371@gmail.com>
2022-05-12 17:43:50 +0800

    新增 mongoDB models

---
+++ models/postsModel.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const postSchema = new mongoose.Schema(
+    {
+      name: {
+        type: String,
+        required: [true, "貼文姓名未填寫"]
+      },
+      content: {
+        type: String,
+        required: [true, 'Content 未填寫']
+      },
+      tags: [
+        {
+          type: String,
+          required: [true, "貼文標籤 tags 未填寫"]
+        }
+      ],    
+      type: {
+        type: String,
+        enum:["group","person"],
+        required: [true, "貼文類型 type 未填寫"]
+      },
+      image: {
+        type:String,
+        default:""
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        select: false 
+      },
+      user: {
+          type: mongoose.Schema.ObjectId,
+          ref:"user",
+          required: [true, '貼文 ID 未填寫']
+      },
+      likes: {
+          type:Number,
+          default:0
+        }
+    }
+);
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
