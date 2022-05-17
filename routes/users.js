738e45449d54a2b47ed6e93126ab16029b9e9ec8
Johnny <asd28618371@gmail.com>
2022-05-17 21:28:56 +0800

    新增 登入驗證

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const UsersContollers = require("../controller/users");
+const { checkAuth, generateSendJWT } = require('../service/auth');
 
 // 取得使用者
 router.get('/', (req, res, next) => {
@@ -14,21 +15,190 @@ router.get('/', (req, res, next) => {
               message: [{
                   _id: '6277b20ad980d4df45db6447',
                   name: 'Johnny',
-                  photo: '123.png'
+                  photo: 'https://i.imgur.com/kti21m2.jpg'
               }]
           }
       }
    */
     UsersContollers.getUsers(req, res, next);
 });
+// 註冊
+router.post('/register', (req, res, next) => {
+  /**
+   * #swagger.tags = ['Users - 使用者']
+   * #swagger.description = '使用者註冊 API'
+   * #swagger.parameters['body'] = {
+      in: 'body',
+      type: 'object',
+      required: true,
+      description: '資料格式',
+      schema: {
+        $name: '小明',
+        $email: '123@gmail.com',
+        $password: '12345678',
+        $confirmPassword: '12345678'
+      }
+    }
+   * #swagger.responses[200] = {
+      description: '註冊資訊',
+      schema: {
+        "status": "success",
+          "user": {
+            "token": "eyJhbGci...",
+            "name": "小明"
+          }
+        }
+      }
+ */
+  UsersContollers.register(req, res, next);
+});
 
-router.get('/login', function(req, res, next) {
+// 登入
+router.post('/login', function(req, res, next) {
+  /**
+   * #swagger.tags = ['Users - 使用者']
+   * #swagger.description = '使用者登入 API'
+   * #swagger.parameters['body'] = {
+      in: 'body',
+      type: 'object',
+      required: true,
+      description: '資料格式',
+      schema: {
+        $email: '123@gmail.com',
+        $password: '12345678',
+      }
+    }
+   * #swagger.responses[200] = {
+      description: '註冊資訊',
+      schema: {
+        "status": "success",
+        "user": {
+          "token": "eyJhbGci...",
+          "name": "小明"
+        }
+      }
+    }
+ */
+    UsersContollers.login(req, res, next);
+});
+
+// 取得個人資訊
+router.get('/profile', checkAuth, function(req, res, next) {
+  /**
+   * #swagger.tags = ['Users - 使用者']
+   * #swagger.description = '取得個人資訊 API'
+   * #swagger.parameters['Authorization'] = {
+      in: 'header',
+      type: 'string',
+      required: true,
+      description: 'Bearer token'
+    }
+   * #swagger.responses[200] = {
+      description: '註冊資訊',
+      schema: {
+        "status": "success",
+        "message": {
+          "_id": "628fd55f474da4b3b4561323",
+          "name": "小明",
+          "sex": "male"
+        }
+      }
+    }
+ */
+  UsersContollers.getOwnProfile(req, res, next);
+});
+
+// 修改個人資訊
+router.patch('/profile', checkAuth, function(req, res, next) {
+  /**
+   * #swagger.tags = ['Users - 使用者']
+   * #swagger.description = '修改個人資訊 API'
+   * #swagger.parameters['Authorization'] = {
+      in: 'header',
+      type: 'string',
+      required: true,
+      description: 'Bearer token'
+    }
+   * #swagger.responses[200] = {
+      description: '註冊資訊',
+      schema: {
+        "status": "success",
+        "message": {
+          "_id": "628fd55f474da4b3b4561323",
+          "name": "小明",
+          "sex": "male"
+        }
+      }
+    }
+ */
+  UsersContollers.updateProfile(req, res, next);
+});
+
+// 取得其他使用者資訊
+router.get('/profile/:id', checkAuth, function(req, res, next) {
     /**
-   * #swagger.ignore = true
-   */
-  res.status(200).json({
-    "name":"login"
-  })
+     * #swagger.tags = ['Users']
+     * #swagger.description = '查看其他用戶資訊'
+     * #swagger.parameters['Authorization'] = {
+            in: 'header',
+            type: 'string',
+            required: true,
+            description: 'Bearer token'
+        }
+     * #swagger.parameters['id'] = {
+            in: 'path',
+            type: 'string',
+            required: true,
+            description: '用戶ID'
+        }
+     * #swagger.responses[200] = {
+            description: 'user 資訊',
+            schema: {
+              status: 'success',
+              "message": {
+                "_id": "628fd55f474da4b3b4fmg323",
+                "name": "小明",
+                "sex": "male"
+              }
+            }
+        }
+     */
+  UsersContollers.getProfileById(req, res, next);
+});
+
+// 修改密碼
+router.patch('/updatePassword', checkAuth, function(req, res, next) {
+    /**
+     * #swagger.tags = ['Users']
+     * #swagger.description = '更改密碼 API'
+     * #swagger.parameters['Authorization'] = {
+            in: 'header',
+            type: 'string',
+            required: true,
+            description: 'Bearer token'
+        }
+     * #swagger.parameters['body'] = {
+            in: 'body',
+            type: 'object',
+            required: true,
+            description: '資料格式',
+            schema: {
+                $password: '12345678',
+                $confirmPassword: '12345678'
+            }
+        }
+     * #swagger.responses[200] = {
+            description: 'user 資訊',
+            schema: {
+              "status": "success",
+              "user": {
+                "token": "eyJhbGci...",
+                "name": "小明"
+              }
+            }
+        }
+     */
+  UsersContollers.updatePassword(req, res, next);
 });
 
 module.exports = router;
