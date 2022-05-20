70596f08e422d25303ece13e7310a02a7b5bcdfe
Johnny <asd28618371@gmail.com>
2022-05-20 16:46:25 +0800

    修改 model Schema

--- models/postsModel.js
+++ models/postsModel.js
@@ -1,32 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema(
-    {
-      content: {
-        type: String,
-        required: [true, '貼文內容 Content 未填寫']
-      },
-      image: {
-        type:String,
-        default:""
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        select: true 
-      },
-      user: {
-          type: mongoose.Schema.ObjectId,
-          ref:"user",
-          required: [true, '貼文 ID 未填寫']
-      },
-      likes: {
-          type:Number,
-          default:0
-        }
+  {
+    content: {
+      type: String,
+      required: [true, '貼文內容 Content 未填寫'],
     },
-    {
-      versionKey: false,
-    }
+    image: {
+      type: String,
+      default: '',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: true,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'user',
+      required: [true, '貼文 ID 未填寫'],
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    versionKey: false,
+  }
 );
 const Post = mongoose.model('Post', postSchema);
 
--- models/usersModel.js
+++ models/usersModel.js
@@ -1,36 +1,40 @@
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
       type: String,
-      required: [true, '請輸入您的名字']
+      required: [true, '請輸入您的名字'],
+      trim: true,
     },
     email: {
       type: String,
       required: [true, '請輸入您的 Email'],
       unique: true,
       lowercase: true,
-      select: false
+      trim: true,
+      select: false,
     },
     photo: String,
-    sex:{
+    sex: {
       type: String,
-      enum:["male","female"]
+      enum: ['male', 'female', ''],
     },
-    password:{
+    password: {
       type: String,
-      required: [true,'請輸入密碼'],
+      required: [true, '請輸入密碼'],
       minlength: 8,
-      select: false
+      select: false,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      select: false
-    }
+      select: false,
+    },
   },
   {
     versionKey: false,
-  });
+  }
+);
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
