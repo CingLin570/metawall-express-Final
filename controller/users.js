a83dc92c0b6659951fbea606e72c6ad8a7d0ad62
Johnny <asd28618371@gmail.com>
2022-06-06 21:39:36 +0800

    新增 按讚列表、取得使用者以及自己的個人資訊

--- controller/users.js
+++ controller/users.js
@@ -6,25 +6,14 @@ const { generateSendJWT } = require('../service/auth');
 const { successHandle } = require('../service/responseHandler');
 const appError = require('../service/appError');
 const handleErrorAsync = require('../service/handleErrorAsync');
+const checkMongoObjectId = require('../service/checkMongoObjectId');
 
 const users = {
-  getOwnProfile: handleErrorAsync(async (req, res, next) => {
-    successHandle(res, req.user);
-  }),
-  getLikeList: handleErrorAsync(async (req, res, next) => {
-    const likeList = await Post.find({
-      likes: { $in: [req.user.id] }
-    }).populate({
-      path:"user",
-      select:"name _id"
-    }).sort('-createdAt');
-    successHandle(res, likeList);
-  }),
   register: handleErrorAsync(async (req, res, next) => {
     let { email, password, confirmPassword, name } = req.body;
     // 內容不可為空
     if (!email || !password || !confirmPassword || !name) {
-      return next(appError('400', '欄位未填寫正確！', next));
+      return appError(400, '欄位未填寫正確！', next);
     }
     const errorMessageArr = [];
     // 密碼是否正確
@@ -55,12 +44,12 @@ const users = {
     }
     if (errorMessageArr.length > 0) {
       errorMessage = errorMessageArr.join(', ');
-      return next(appError(400, errorMessage, next));
+      return appError(400, errorMessage, next);
     }
     // 查詢是否存在信箱
     const findUserByMail = await User.findOne({ email });
     if (findUserByMail) {
-      return appError('email 已註冊', 400, next);
+      return appError(400, 'email 已註冊', next);
     }
     // 加密密碼
     password = await bcrypt.hash(req.body.password, 12);
@@ -75,26 +64,74 @@ const users = {
   }),
   login: handleErrorAsync(async (req, res, next) => {
     const { email, password } = req.body;
+    const errorMessageArr = [];
+    // 帳號密碼不可為空
     if (!email || !password) {
-      return next(appError(400, '登入失敗，帳號密碼不可為空', next));
+      return appError(400, '帳號密碼不可為空', next);
+    }
+    // 是否為 Email
+    if (!validator.isEmail(email)) {
+      errorMessageArr.push('登入失敗，Email 格式不正確');
+    }
+    // 密碼需符合八碼以上並英數混合，至少各一個大小寫英文字母
+    if (!validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 0,
+      })
+    ) {
+      errorMessageArr.push(
+        '登入失敗，密碼需符合八碼以上並英數混合，至少各一個大小寫英文字母'
+      );
+    }
+    if (errorMessageArr.length > 0) {
+      errorMessage = errorMessageArr.join(', ');
+      return appError(400, errorMessage, next);
     }
     const user = await User.findOne({ email }).select('+password');
     if (!user) {
-      return appError('登入失敗，帳號錯誤或尚未註冊', 400, next);
+      return appError(400, '登入失敗，帳號錯誤或尚未註冊', next);
     }
     const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
-      return next(appError(400, '登入失敗，您的密碼不正確', next));
+      return appError(400, '登入失敗，您的密碼不正確', next);
     }
     generateSendJWT(user, 200, res);
   }),
+  getOwnProfile: handleErrorAsync(async (req, res, next) => {
+    successHandle(res, req.user);
+  }),
+  getUserProfile: handleErrorAsync(async (req, res, next) => {
+    const otherUserId = req.params.id;
+    const userId = req.user.id;
+    if(!checkMongoObjectId(otherUserId)) {
+      return appError(400, '取得失敗，請輸入正確的ID格式', next);
+    }
+    const findUser = await User.findById(otherUserId);
+    if(!findUser) {
+      return appError(400, '取得失敗，查無此使用者ID', next);
+    }
+    const { name, photo, followers } = findUser
+    followerLength = followers.length;
+    const checkFollow = await User.findOne({
+      _id: otherUserId,
+      'followers.user': { $in: userId }
+    }).count().exec();
+    successHandle(res, { name, photo, followerLength, checkFollow });
+  }),
   updateProfile: handleErrorAsync(async (req, res, next) => {
+    const userId = req.user.id;
     const { name, sex, photo } = req.body;
     if (!name) {
-      next(appError(400, '更新失敗，姓名必填欄位未填寫', next));
+      return appError(400, '更新失敗，姓名必填欄位未填寫', next);
+    }
+    if (!sex) {
+      return appError(400, '更新失敗，性別必填欄位未填寫', next);
     }
     const updateUser = await User.findByIdAndUpdate(
-      req.user.id,
+      userId,
       {
         $set: {
           name,
@@ -111,14 +148,18 @@ const users = {
   }),
   updatePassword: handleErrorAsync(async (req, res, next) => {
     const { password, confirmPassword } = req.body;
+    const userId = req.user.id;
     const errorMessageArr = [];
+    // 密碼與確認密碼不可為空
+    if (!password || !confirmPassword) {
+      return appError(400, '密碼與確認密碼不可為空', next);
+    }
     // 密碼是否正確
     if (password !== confirmPassword) {
       errorMessageArr.push('密碼不一致！');
     }
     // 密碼需符合八碼以上並英數混合，至少各一個大小寫英文字母
-    if (
-      !validator.isStrongPassword(password, {
+    if (!validator.isStrongPassword(password, {
         minLength: 8,
         minLowercase: 1,
         minUppercase: 1,
@@ -132,20 +173,33 @@ const users = {
     }
     if (errorMessageArr.length > 0) {
       errorMessage = errorMessageArr.join(', ');
-      return next(appError(400, errorMessage, next));
+      return appError(400, errorMessage, next);
     }
     newPassword = await bcrypt.hash(password, 12);
 
-    const user = await User.findByIdAndUpdate(req.user.id, {
+    const user = await User.findByIdAndUpdate(userId, {
       password: newPassword,
     });
     generateSendJWT(user, 200, res);
   }),
+  getLikeList: handleErrorAsync(async (req, res, next) => {
+    const userId = req.user.id;
+    const likeList = await Post.find({
+      likes: { $in: [userId] }
+    }).populate({
+      path:"user",
+      select:"name _id photo"
+    }).sort('-createdAt');
+    successHandle(res, likeList);
+  }),
   createFollow: handleErrorAsync(async (req, res, next) => {
-    const user = req.user.id;
+    const userId = req.user.id;
     const followUser = req.params.id
-    if(followUser === user) {
-      return next(appError(400, '新增失敗，您不能追蹤自己', next));
+    if(!checkMongoObjectId(followUser)) {
+      return appError(400, '新增失敗，請輸入正確的ID格式', next);
+    }
+    if(followUser === userId) {
+      return appError(400, '新增失敗，您不能追蹤自己', next);
     }
     const findUser = await User.findById(followUser);
     if(!findUser) {
@@ -153,7 +207,7 @@ const users = {
     }
     await User.updateOne(
       {
-        _id: user,
+        _id: userId,
         'following.user': { $ne: followUser }
       },
       {
@@ -163,19 +217,22 @@ const users = {
     await User.updateOne(
       {
         _id: followUser,
-        'followers.user': { $ne: user }
+        'followers.user': { $ne: userId }
       },
       {
-        $addToSet: { followers: { user: user } }
+        $addToSet: { followers: { user: userId } }
       }
     );
     successHandle(res, '新增追蹤成功');
   }),
   deleteFollow: handleErrorAsync(async (req, res, next) => {
-    const user = req.user.id;
+    const userId = req.user.id;
     const followUser = req.params.id
-    if(followUser === user) {
-      return next(appError(400, '刪除失敗，您不能取消追蹤自己', next));
+    if(!checkMongoObjectId(followUser)) {
+      return appError(400, '新增失敗，請輸入正確的ID格式', next);
+    }
+    if(followUser === userId) {
+      return appError(400, '刪除失敗，您不能取消追蹤自己', next);
     }
     const findUser = await User.findById(followUser);
     if(!findUser) {
@@ -183,7 +240,7 @@ const users = {
     }
     await User.updateOne(
       {
-        _id: user,
+        _id: userId,
         'following.user': { $eq: followUser }
       },
       {
@@ -193,16 +250,17 @@ const users = {
     await User.updateOne(
       {
         _id: followUser,
-        'followers.user': { $eq: user }
+        'followers.user': { $eq: userId }
       },
       {
-        $pull: { followers: { user: user } }
+        $pull: { followers: { user: userId } }
       }
     );
     successHandle(res, '取消追蹤成功');
   }),
   getFollowing: handleErrorAsync(async (req, res, next) => {
-    const user = await User.findById(req.user.id)
+    const userId = req.user.id;
+    const findUser = await User.findById(userId)
     .populate({
       path: 'following',
       populate: { 
@@ -211,7 +269,7 @@ const users = {
       },
       options: { sort: '-createdAt' }
     });
-    successHandle(res, user);
+    successHandle(res, findUser);
   })
 };
 
