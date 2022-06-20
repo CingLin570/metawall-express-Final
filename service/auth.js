d3d611904fc40c4a8d6c9ca719843178dd93ca8f
Johnny <asd28618371@gmail.com>
2022-06-20 17:24:15 +0800

    新增 第三方登入轉址jwt

--- service/auth.js
+++ service/auth.js
@@ -45,7 +45,15 @@ const generateSendJWT = (user, statusCode, res) => {
   });
 };
 
+const generateUrlJWT = (user) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_DAY,
+  });
+  return token
+}
+
 module.exports = {
   checkAuth,
   generateSendJWT,
+  generateUrlJWT
 };
