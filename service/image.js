fc8a5995b0a3981b36cfddd1143b239c8324e7da
Johnny <asd28618371@gmail.com>
2022-06-24 15:41:33 +0800

    新增 加入上傳檔案大小限制錯誤回傳

--- service/image.js
+++ service/image.js
@@ -1,16 +1,27 @@
 const multer = require('multer');
+const appError = require('../service/appError');
+const handleErrorAsync = require('../service/handleErrorAsync');
 
-const upload = multer({
-  limits: {
-    fileSize: 1 * 1024 * 1024,
-  },
-  fileFilter(req, file, cb) {
-    // 只接受三種圖片格式
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。'));
+const uploadFile = handleErrorAsync(async (req, res, next) => {
+  const upload = multer({
+    limits: {
+      fileSize: 1 * 1024 * 1024,
+    },
+    fileFilter(req, file, cb) {
+      // 只接受三種圖片格式
+      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        cb(new Error('檔案格式錯誤，僅限上傳 jpg、jpeg 與 png 格式。'));
+      }
+      cb(null, true);
+    },
+  }).single('image');
+  upload(req, res, (err) => {
+    if(err instanceof multer.MulterError) {
+      return appError(400, '上傳失敗，圖片檔案必須小於1MB', next);
+    } else if(err) {
+      return appError(400, err, next);
     }
-    cb(null, true);
-  },
-}).single('image');
+  })
+})
 
-module.exports = upload;
+module.exports = uploadFile;
