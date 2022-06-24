9ea0a2e4bc1e56730f6f6084d7c74554cb62f5be
Johnny <asd28618371@gmail.com>
2022-06-24 11:03:27 +0800

    新增 env

--- service/passport.js
+++ service/passport.js
@@ -5,7 +5,7 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://still-fortress-89062.herokuapp.com/auth/google/callback",
+    callbackURL: `${process.env.CALLBACKURL}/auth/google/callback`,
   },
   (accessToken, refreshToken, profile, cb) => {
     return cb(null, profile._json);
@@ -15,7 +15,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "https://still-fortress-89062.herokuapp.com/auth/facebook/callback",
+  callbackURL: `${process.env.CALLBACKURL}/auth/facebook/callback`,
   profileFields: ['id', 'displayName', 'photos', 'email'],
 },
 (accessToken, refreshToken, profile, cb) => {
